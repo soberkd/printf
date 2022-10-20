69d03de87597d9955ba626de815ab9bde182277e
soberkd <soberkd6@gmail.com>
2022-10-20 11:08:09 +0000

    _printf main

--- _printf.c
+++ _printf.c
@@ -5,13 +5,13 @@
  * @format: possible format specifier
  *
  * Return: pointer to valid function or NULL
-*/
+ */
 static int (*check_for_specifiers(const char *format))(va_list)
 {
 	unsigned int i;
 
 	print_t p[] = {
-		"c", print_c},
+		{"c", print_c},
 		{"s", print_s},
 		{"i", print_i},
 		{"d", print_d},
@@ -42,7 +42,7 @@ static int (*check_for_specifiers(const char *format))(va_list)
  * @format: list of argument types passed to the function
  *
  * Return: number of characters printed
-*/
+ */
 int _printf(const char *format, ...)
 {
 	unsigned int i = 0, count = 0;
